f753b117476663165b9b70b544f38b5eb4a33f02
scamlett <scamlett@student.42malaga.com>
2026-03-14 14:18:28 +0100

    Update ex00 with modified source files

--- ex00/check.c
+++ ex00/check.c
@@ -6,13 +6,13 @@
 /*   By: scamlett <scamlett@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/14 12:14:15 by scamlett          #+#    #+#             */
-/*   Updated: 2026/03/14 13:05:43 by scamlett         ###   ########.fr       */
+/*   Updated: 2026/03/14 14:09:48 by scamlett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "rush-01.h"
+#include <unistd.h>
 
-static int	contar_visibles(int linea[SIZE])
+int	contar_visibles(int linea[4])
 {
 	int	i;
 	int	maximo;
@@ -21,7 +21,7 @@ static int	contar_visibles(int linea[SIZE])
 	i = 0;
 	visibles = 0;
 	maximo = 0;
-	while (i < SIZE)
+	while (i < 4)
 	{
 		if (linea[i] > maximo)
 		{
@@ -33,66 +33,65 @@ static int	contar_visibles(int linea[SIZE])
 	return (visibles);
 }
 
-static void	rellenar_linea(int tablero[SIZE][SIZE], int linea[SIZE], int i,
-		int modo)
+void	rellenar_linea(int tablero[4][4], int linea[4], int i, int modo)
 {
 	int	j;
 
 	j = 0;
-	while (j < SIZE)
+	while (j < 4)
 	{
 		if (modo == 0)
 			linea[j] = tablero[j][i];
 		else if (modo == 1)
-			linea[j] = tablero[SIZE - 1 - j][i];
+			linea[j] = tablero[4 - 1 - j][i];
 		else if (modo == 2)
 			linea[j] = tablero[i][j];
 		else
-			linea[j] = tablero[i][SIZE - 1 - j];
+			linea[j] = tablero[i][4 - 1 - j];
 		j++;
 	}
 }
 
-int	comprobar_vistas(int tablero[SIZE][SIZE], int *pistas)
+int	comprobar_vistas(int tablero[4][4], int *pistas)
 {
 	int	i;
-	int	linea[SIZE];
+	int	linea[4];
 
 	i = 0;
-	while (i < SIZE)
+	while (i < 4)
 	{
 		rellenar_linea(tablero, linea, i, 0);
 		if (contar_visibles(linea) != pistas[i])
 			return (0);
 		rellenar_linea(tablero, linea, i, 1);
-		if (contar_visibles(linea) != pistas[SIZE + i])
+		if (contar_visibles(linea) != pistas[4 + i])
 			return (0);
 		rellenar_linea(tablero, linea, i, 2);
-		if (contar_visibles(linea) != pistas[(SIZE * 2) + i])
+		if (contar_visibles(linea) != pistas[(4 * 2) + i])
 			return (0);
 		rellenar_linea(tablero, linea, i, 3);
-		if (contar_visibles(linea) != pistas[(SIZE * 3) + i])
+		if (contar_visibles(linea) != pistas[(4 * 3) + i])
 			return (0);
 		i++;
 	}
 	return (1);
 }
 
-void	imprimir_tablero(int tablero[SIZE][SIZE])
+void	imprimir_tablero(int tablero[4][4])
 {
 	int		fila;
 	int		columna;
 	char	c;
 
 	fila = 0;
-	while (fila < SIZE)
+	while (fila < 4)
 	{
 		columna = 0;
-		while (columna < SIZE)
+		while (columna < 4)
 		{
 			c = tablero[fila][columna] + '0';
 			write(1, &c, 1);
-			if (columna < SIZE - 1)
+			if (columna < 4 - 1)
 				write(1, " ", 1);
 			columna++;
 		}
